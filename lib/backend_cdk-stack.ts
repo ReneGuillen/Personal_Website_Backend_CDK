54586ca095f21ebf1c3bd63fd1b3d2b5069d3ba9
Josue <renguill47@gmail.com>
2023-10-27 19:38:51 -0500

    Initial commit

---
+++ lib/backend_cdk-stack.ts
@@ -0,0 +1,14 @@
+import * as cdk from 'aws-cdk-lib';
+import { aws_s3 as s3 } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class BackendCdkStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    new s3.Bucket(this, 'MyFirstBucket', {
+      versioned: true
+    })
+  }
+}
